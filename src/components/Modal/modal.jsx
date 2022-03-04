9f9a98dfeb5c14ebfb3b4afaa9ae3f12319ce48d
marcospsd <marcosp_sd@hotmail.com>
2022-03-04 17:08:24 -0300

    visualização rolando ...

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -3,62 +3,114 @@ import "./modal.css"
 import Box from '@mui/material/Box';
 import IconButton  from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ArticleIcon from '@mui/icons-material/Article';
+import PrintIcon from '@mui/icons-material/Print';
+import EditIcon from '@mui/icons-material/Edit';
 import Modal from '@mui/material/Modal';
 import { useFetch } from '../../hooks/useFetch';
 import { deleteVendas } from "../../services/api";
 
+import { mutate as mutateGlobal } from 'swr';
 
 
-
-  const BasicModal = () => {
-    const { data, mutate } = useFetch(`api/v2/venda/1`)
+  const BasicModal = (id) => {
+    const {data, mutate} = useFetch(`/api/v2/venda/${id.value}`)
     const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    if (!data) {
+      return <button onClick={handleOpen}>Open modal</button>
+    }
 
-  //   function AcaoDeletar (id) {
-  //     if ( window.confirm("Deseja deletar essa venda ?")) {
-  //         deleteVendas(id);
-  //         const refreshVendas = data.filter((venda) => venda.ordem !== id)
-  //         mutate(refreshVendas, false)
-  //     }
-  // }
+
+    function AcaoDeletar (id) {
+      if ( window.confirm("Deseja deletar essa venda ?")) {
+          deleteVendas(id);
+          mutateGlobal(`/api/v2/venda/`, !data)
+          handleClose()
+      }
+  }
 
 
     return (
       <div>
-          <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description">
-          { data.map((venda) => (
-            <Box id="box-view-itens" key={venda.ordem}>
-            <p><strong>Ordem: </strong>{venda.ordem}</p>
-            <p><strong>CPF: </strong>{(venda.nome_cliente).replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")}</p>
-            <p><strong>Total da Venda: </strong>{parseInt(venda.total_venda).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</p>
-            {venda.formavenda.map((formpag) => (
-                <div className='formavenda-css' key={formpag.id}>
-                <p><strong>Tipo: </strong>{formpag.forma} || <strong>Parcelas: </strong>{formpag.parcelas} || <strong>Valor: </strong>{formpag.valor}</p>
-                </div>
-            ))}
-            {venda.corpovenda.map((corpovenda) => (
-                <div className='corpovenda-css' key={corpovenda.id}>
-                    <p><strong>Codigo: </strong> {corpovenda.codpro}</p>
-                    <p><strong>Descricao: </strong> Blablabla</p>
-                    <p><strong>De:</strong>{corpovenda.valor_unitsis}</p>
-                    <p><strong>Por: </strong>{corpovenda.valor_unitpro}</p>
-                    <p><strong>Quantidade: </strong>{corpovenda.quantidade}</p>
-                </div>
-            ))}
-                {/* <IconButton aria-label="delete" size="large" id="excluir" onClick={() => AcaoDeletar(venda.ordem)}>
-                    <DeleteIcon />
-                </IconButton> */}
-                </Box>
-          ))}
-          </Modal>
-      </div>
-    );
+      <IconButton onClick={handleOpen} id='excluir'><ArticleIcon /></IconButton>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box id='box-view-itens'>
+            <div id='tittle-modal'>
+              <label><strong>RESUMO DA VENDA</strong></label>
+            </div>
+            <div id='dadosvenda'>
+              <div className='col'>
+                <label><strong>Ordem: </strong> {data.ordem}</label>
+                <label><strong>CPF: </strong> {(data.nome_cliente).replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")}</label>
+              </div>
+              <div className='col'>
+              <label><strong>Vendedor: </strong> {data.vendedor}</label>
+              <label><strong>Total da Venda: </strong> {parseInt(data.total_venda).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</label>
+              </div>
+            </div>
+            <div id='formadepagamento'>
+            <p><strong>FORMA DE PAGAMENTO</strong></p>
+              <table>
+                <tbody>
+                <tr id='formatitle'>
+                  <td>Forma de Pagamento</td>
+                  <td>Parcelas</td>
+                  <td>Valor</td>
+                </tr>
+              {data.formavenda.map((forma) => (
+                <tr key={forma.id}>
+                  <td className='formaconteudo' >{forma.forma}</td>
+                  <td className='formaconteudo' >{forma.parcelas}</td>
+                  <td className='formaconteudo' >{parseInt(forma.valor).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</td>
+                </tr>
+              ))}
+              </tbody>
+              </table>
+            </div>
+            <div id='corpovenda'>
+              <p><strong>PRODUTOS</strong></p>
+              <table>
+                <tbody>
+                <tr id='corpotittle'>
+                    <td>Codigo</td>
+                    <td>Quantidade</td>
+                    <td>Valor Unitário</td>
+                    <td>Valor Final</td>
+                    
+                </tr>
+              {data.corpovenda.map((corpovenda) => (
+                <tr key={corpovenda.id}>
+                <td>{corpovenda.codpro}</td>
+                <td>{corpovenda.quantidade}</td>
+                <td>{parseInt(corpovenda.valor_unitsis).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</td>
+                <td>{parseInt(corpovenda.valor_unitpro).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}</td>
+                </tr>
+              ))}
+              </tbody>
+              </table>
+            </div>
+            <div id='opcoes'>
+              <IconButton><EditIcon/></IconButton>
+
+              <IconButton><PrintIcon/></IconButton>
+
+              <IconButton onClick={() => AcaoDeletar(data.ordem)} id='delete'><DeleteIcon/></IconButton>
+            </div>
+
+
+
+        </Box>
+      </Modal>
+    </div>
+  );
 }
 
 export default BasicModal;
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "http://10.3.1.95:80",
 });
 
 export const createSession = (username, password) => {
