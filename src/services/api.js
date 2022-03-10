c559d0fdd98a2f2ef121dbec6cea0f1d5e561f32
marcospsd <marcosp_sd@hotmail.com>
2022-03-10 17:33:40 -0300

    api publica

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL: "http://10.3.1.95:80",
+    baseURL: "http://177.53.172.226:2000",
 });
 
 export const createSession = (username, password) => {
