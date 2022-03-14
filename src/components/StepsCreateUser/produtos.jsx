3f8fc2a840e0af2cf67b5dcca36e6ff1a350c245
marcospsd <marcosp_sd@hotmail.com>
2022-03-14 13:07:48 -0300

    feito

--- src/components/StepsCreateUser/produtos.jsx
+++ src/components/StepsCreateUser/produtos.jsx
@@ -1,40 +1,111 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import Button from '@mui/material/Button';
 import IconButton  from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { api } from '../../services/api';
+import CircularProgress from '@mui/material/CircularProgress';
+
+
 
 export const ProdutosForm = ({ formData, setForm, navigation }) => {
+    const [codpro, setCodPro] = useState("")
+    const [descripro, setDescriPro] = useState("")
+    const [valorsis, setValorSis] = useState("")
+    const [valorpro, setValorPro] = useState("")
+    const [pesquisa, setPesquisa] = useState("")
+    const [resultado, setResultado] = useState([])
+
+
+    const Adicionar = () => {
+        if (codpro !== "") {
+        setForm({...formData, corpovenda: [
+            ... formData.corpovenda,
+            { 
+            codpro: codpro, 
+            descripro: descripro, 
+            quantidade: 1,
+            valor_unitsis: valorsis,
+            valor_unitpro: valorpro,
+        },
+        ],
+    }
+    )
+        setCodPro("")
+        setDescriPro("")
+        setValorSis("")
+        setValorPro("")
+        setPesquisa("")
+        setResultado([])
+        } else {
+            window.alert("não pode adicionar vazio !")
+        }
+    }
+
+
+    const Pesquisar = async (pesquisa) => {
+        if (pesquisa !== "") {
+            api.get(`/api/v1/produto/${pesquisa}`)
+            .then((res) => {setResultado(res.data)})
+        }
+        return;
+    }
+
+
+    console.log(formData)
+    console.log(valorpro)
+
     return (
         <div className='container-products'>
             <p>Produtos</p>
             <div className='label-products'>
                 <div className='col-search-products'>
-                <TextField label='Pesquise pelo Codigo ou Descricao'/>
+                <Autocomplete
+                    disablePortal
+                    id="combo-box-demo"
+                    getOptionLabel={(resultados) => `${resultados.codigo} - ${resultados.descricao}`}
+                    onChange = {(resultado, newResultado) => {
+                        if (newResultado.codigo !== "") {
+                            setCodPro(newResultado.codigo)
+                            setDescriPro(newResultado.descricao)
+                            setValorSis(newResultado.valor_unitsis)
+                            setValorPro(newResultado.valor_unitpro)
+                        }
+                        else { return; }
+                    }}
+                    options={resultado}
+                    renderInput={(params) => <TextField {...params} label="Pesquise pela Descrição do Produto" onChange={(e) => Pesquisar(e.target.value)} value={pesquisa} />}
+                    />
+                <p></p>
                 </div>
                 <div className='col-products'>
-                <TextField label='Codigo' disabled/>
-                <TextField label='Descricao' disabled/>
+                <TextField label='Codigo' value={codpro} disabled/>
+                <TextField label='Descricao' value={descripro} disabled/>
                 </div>
                 <div className='col-products'>
-                <TextField label='Valor Sistema' disabled/>
-                <TextField label='Valor Promoção' disabled/>
+                <TextField label='Valor Sistema' value={valorsis} disabled/>
+                <TextField label='Valor Promoção' value={valorpro} onChange={(e) => setValorPro(e.target.value)} disabled/>
                 </div>
                 
             </div>
-            <Button id='back' variant="contained">Adicionar</Button>
-            <p></p>
-            <div className='card-products'>
-                <label id='Codigo'><strong>Codigo: </strong>11122233</label>
-                <label id='Descricao'><strong>Descricao: </strong>Oc Rban XXXXX xXXXX </label>
-                <label id='Valor Sistema'><strong>Valor Sistema: </strong>R$ 1520,00</label>
-                <label id='Valor Promoção'><strong>Valor Promoção: </strong>R$ 600,00</label>
-                <IconButton id='delete'><DeleteIcon/></IconButton>
+            <Button id='back' variant="contained" onClick={Adicionar}>Adicionar</Button>
+            {formData.corpovenda.map((venda) =>
+            <div className='card-products' key={venda.codpro}>
+                <label id='Codigo'><strong>Codigo: </strong>{venda.codpro}</label>
+                <label id='Descricao'><strong>Descricao: </strong>{venda.descripro}</label>
+                <label id='Valor Sistema'><strong>Valor Sistema: </strong>{venda.valor_unitsis}</label>
+                <label id='Valor Promoção'><strong>Valor Promoção: </strong>{venda.valor_unitpro}</label>
+                <IconButton id='delete' ><DeleteIcon/></IconButton>
             </div>
+            )}
             <div className='buttons-products'>
                 <Button id='back' onClick={() => navigation.previous()} variant="contained">Back</Button>
-                <Button id='next' onClick={() => navigation.next() } variant="contained">Next</Button>
+                <Button id='next' onClick={() => {
+
+                    navigation.next() 
+
+                }} variant="contained">Next</Button>
             </div>
         </div>
     )
